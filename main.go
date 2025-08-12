7b9e83498462821c5548e37cc2af213bb126c4bd
thinhnv99 <thinhnv0412@gmail.com>
2025-08-12 17:10:51 +0700

    Create minimal web server

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "ok",
+		})
+	})
+
+	r.Run(":8088")
+}
